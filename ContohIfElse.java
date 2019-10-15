c6b301914269685c3c339312217439b826cd09e1
warokdroid2 <56612320+warokdroid2@users.noreply.github.com>
2019-10-16 05:28:31 +0700

    Create ContohIfElse.java

---
+++ ContohIfElse.java
@@ -0,0 +1,13 @@
+public class ContohIfElse {
+
+    public static void main(String[] args) {
+
+        int nilai = 50;
+
+        if (nilai >= 80 && nilai <= 100) {
+            System.out.println("Mendapat nilai A");
+        } else {
+            System.out.println("Nilai selain A");
+        }
+    }
+}
